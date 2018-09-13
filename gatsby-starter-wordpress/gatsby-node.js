5e445c7dd24ce917649484fa0dab228af25a0398
Callum Macdonald <git@callum-macdonald.com>
2018-09-13 16:35:03 +0200

    Prettier.

--- gatsby-starter-wordpress/gatsby-node.js
+++ gatsby-starter-wordpress/gatsby-node.js
@@ -1,14 +1,14 @@
-const _ = require(`lodash`)
-const Promise = require(`bluebird`)
-const path = require(`path`)
-const slash = require(`slash`)
-const webpackLodashPlugin = require('lodash-webpack-plugin')
+const _ = require(`lodash`);
+const Promise = require(`bluebird`);
+const path = require(`path`);
+const slash = require(`slash`);
+const webpackLodashPlugin = require("lodash-webpack-plugin");
 
 // Will create pages for Wordpress pages (route : /{slug})
 // Will create pages for Wordpress posts (route : /{slug})
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators
+  const { createPage } = boundActionCreators;
   return new Promise((resolve, reject) => {
     // First, query all the pages on your WordPress
     graphql(
@@ -29,11 +29,11 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
     )
       .then(result => {
         if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
+          console.log(result.errors);
+          reject(result.errors);
         }
         // Create those pages with the wp_page.jsx template.
-        const pageTemplate = path.resolve(`./src/templates/wp_page.jsx`)
+        const pageTemplate = path.resolve(`./src/templates/wp_page.jsx`);
         _.each(result.data.allWordpressPage.edges, edge => {
           createPage({
             path: `/${edge.node.slug}/`,
@@ -41,8 +41,8 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
             context: {
               id: edge.node.id
             }
-          })
-        })
+          });
+        });
       })
       // Now, querying all wordpressPosts
       .then(() => {
@@ -68,23 +68,23 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
           `
         ).then(result => {
           if (result.errors) {
-            console.log(result.errors)
-            reject(result.errors)
+            console.log(result.errors);
+            reject(result.errors);
           }
-          const tags = []
-          const categories = []
-          const postTemplate = path.resolve(`./src/templates/post.jsx`)
+          const tags = [];
+          const categories = [];
+          const postTemplate = path.resolve(`./src/templates/post.jsx`);
           // We want to create a detailed page for each
           // post node. We'll just use the Wordpress Slug for the slug.
           // The Post ID is prefixed with 'POST_'
           _.each(result.data.allWordpressPost.edges, edge => {
             // grab all the tags and categories for later use
             edge.node.tags.forEach(tag => {
-              tags.push(tag.name)
-            })
+              tags.push(tag.name);
+            });
             edge.node.categories.forEach(category => {
-              categories.push(category.name)
-            })
+              categories.push(category.name);
+            });
 
             createPage({
               path: `/${edge.node.slug}`,
@@ -92,17 +92,17 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
               context: {
                 id: edge.node.id
               }
-            })
-          })
+            });
+          });
           // ==== END POSTS ====
 
           // Create pages for each unique tag and category
-          const tagsTemplate = path.resolve(`./src/templates/tag.jsx`)
+          const tagsTemplate = path.resolve(`./src/templates/tag.jsx`);
           const categoriesTemplate = path.resolve(
             `./src/templates/category.jsx`
-          )
-          const tagsSet = new Set(tags)
-          const catSet = new Set(categories)
+          );
+          const tagsSet = new Set(tags);
+          const catSet = new Set(categories);
           tagsSet.forEach(tag => {
             createPage({
               path: `/tags/${_.kebabCase(tag)}/`,
@@ -110,8 +110,8 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
               context: {
                 id: tag
               }
-            })
-          })
+            });
+          });
 
           catSet.forEach(cat => {
             createPage({
@@ -120,18 +120,18 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
               context: {
                 id: cat
               }
-            })
-          })
-          resolve()
-        })
-      })
+            });
+          });
+          resolve();
+        });
+      });
     // === END TAGS ===
-    resolve()
-  })
-}
+    resolve();
+  });
+};
 
 exports.modifyWebpackConfig = ({ config, stage }) => {
-  if (stage === 'build-javascript') {
-    config.plugin('Lodash', webpackLodashPlugin, null)
+  if (stage === "build-javascript") {
+    config.plugin("Lodash", webpackLodashPlugin, null);
   }
-}
+};
